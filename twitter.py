286ff36c3fb4e2bb348ed326eeb4f4680240b579
Kenta Yamada <kentayamada058@gmail.com>
2022-09-23 06:07:51 +0000

    update

--- twitter.py
+++ twitter.py
@@ -5,24 +5,28 @@ from random import sample
 from tweepy import Client
 from dotenv import load_dotenv
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Chrome as ChromeDriver
+from retry import retry
 from chrome_driver import get_chrome_driver
 
 
 def check_hashtag(string: str):
     return string if "#" in string else f"#{string}"
 
-
-def tweet(client: Client, driver: ChromeDriver):
-    driver.get("https://twittrend.jp/")
+@retry(tries=5, delay=5)
+def tweet(client: Client):
+    chrome_driver = get_chrome_driver()
+    chrome_driver.get("https://twittrend.jp/")
     trend_list = [
-        driver.find_element(By.XPATH, f"//li[{i}]/p/a").text for i in range(1, 21)
+        chrome_driver.find_element(By.XPATH, f"//li[{i}]/p/a").text
+        for i in range(1, 21)
     ]
     trends = [check_hashtag(trend) for trend in sample(trend_list, 2)]
     trends_hashtags = " ".join(trends) + " #最新ニューズピックス"
 
-    driver.get("https://news.yahoo.co.jp/flash")
-    topic = driver.find_element(By.XPATH, "//*[@id='contentsWrap']/div[1]/div[2]/div/a")
+    chrome_driver.get("https://news.yahoo.co.jp/flash")
+    topic = chrome_driver.find_element(
+        By.XPATH, "//*[@id='contentsWrap']/div[1]/div[2]/div/a"
+    )
     topic_text = topic.find_element(By.TAG_NAME, "p").text
     topic_url = topic.get_attribute("href")
 
@@ -37,5 +41,4 @@ if __name__ == "__main__":
         access_token=environ["TWITTER_ACCESS_TOKEN"],
         access_token_secret=environ["TWITTER_ACCESS_TOKEN_SECRET"],
     )
-    chrome_driver = get_chrome_driver()
-    tweet(tweepy_client, chrome_driver)
+    tweet(tweepy_client)
