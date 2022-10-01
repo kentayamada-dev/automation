452010558f91a7524ccdeedb01563efd799482a6
Kenta Yamada <83388735+kentayamada-dev@users.noreply.github.com>
2022-10-01 14:13:36 +0000

    update

--- twitter.py
+++ twitter.py
@@ -1,10 +1,14 @@
 # https://www.youtube.com/watch?v=fD-GRCH_tks
 
 from os import environ
+from datetime import datetime, timedelta, timezone
+from random import choice
 from tweepy import Client
 from dotenv import load_dotenv
 from tenacity import retry, wait_fixed, stop_after_attempt
 from requests_html import HTMLSession
+
+IS_EVEN_MIN = datetime.now(timezone(timedelta(hours=+9), "JST")).minute % 2 == 0
 
 
 def check_hashtag(string):
@@ -13,14 +17,23 @@ def check_hashtag(string):
 
 @retry(stop=stop_after_attempt(5), wait=wait_fixed(10))
 def tweet(client: Client):
-    topic = [
-        response
-        for response in HTMLSession()
-        .get("https://news.yahoo.co.jp/flash")
-        .html.find("a")
-        if "https://news.yahoo.co.jp/articles/" in next(iter(response.links))
-    ][0]
-    topic_text = topic.attrs["aria-label"]
+    data = HTMLSession().get("https://news.yahoo.co.jp/flash").html.find("a")
+    topic = (
+        [
+            response
+            for response in data
+            if "https://news.yahoo.co.jp/articles/" in next(iter(response.links))
+        ][0]
+        if IS_EVEN_MIN
+        else choice(
+            [
+                response
+                for response in data
+                if "https://news.yahoo.co.jp/pickup/" in next(iter(response.links))
+            ]
+        )
+    )
+    topic_text = topic.attrs["aria-label"] if IS_EVEN_MIN else topic.text
     topic_url = topic.attrs["href"]
 
     client.create_tweet(text=f"{topic_text}\n{topic_url}\n\n#最新ニューズピックス")
